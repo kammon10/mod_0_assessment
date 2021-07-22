249dc4daef61b6fb9e23e53b19a34a490daecc3d
Katie Ammon <katie.ammon10@gmail.com>
2021-07-22 14:09:31 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,11 @@
+var famousPerson = "Victor Hugo";
+var isActive = true;
+famousPerson = "Edger Poe";
+var num3 = 5;
+var num9 = 8;
+var sum = (num3 + num9);
+var animals = ["lions", "tigers", "bears"];
+for (var i = 1; i < animals.length; i++)
+console.log(animals[i]);
+var animalsLength=animals.length
+console.log(animalsLength);
