389993efd61050b4931c650b8fa14f5c1bd9624d
Katie Ammon <katie.ammon10@gmail.com>
2021-07-22 14:18:24 -0600

    Add airPort object

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var airPort = {
+  city: "Denver",
+  State: "Colorado",
+  BestDest: ["Utah", "Florida", "Turkey"],
+  WorstDest: ["Ohio", "Indiana", "Texas"],
+  goodExperience: true,
+  likeFlying: true,
+  statesVisited: 25,
+  statesToVisit: 15,
+
+}
+console.log(airPort);
