a4f3865e5aa621523eddd4f5878ad8f7a75e712d
Katie Ammon <katie.ammon10@gmail.com>
2021-07-22 14:25:57 -0600

    Add my

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var myCar = {
+  year: 2007,
+  make: "Toyota",
+  model: "4Runner",
+  seats: 6,
+  sunroof: true,
+  camera: false,
+  features: ["Leather interior", "sound system", "heated seats"],
+  damage: ["Sun damage to front pass seat", "dent in rear bumper"],
+}
+console.log(myCar);
